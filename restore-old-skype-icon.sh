5b27586666dfc9b2cf7c7203e2ac166cc716e28b
Piotr Wittchen <piotr@wittchen.biz.pl>
2015-11-01 22:55:25 +0100

    Update restore-old-skype-icon.sh

--- restore-old-skype-icon.sh
+++ restore-old-skype-icon.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-set -e
+set -e # tells bash, in a script, to exit whenever anything returns a non-zero return value
 
 main() {
   sudo rm -r /usr/share/pixmaps/skype
 }
 
-main "$@"
+main "$@" # "$@" is an array-like construct of all positional parameters, {$1, $2, $3 ...}
